0af21bb0825fda4663140a825776b45182b87f30
MarkosTh09 <markos.theocharis2009@gmail.com>
2024-05-01 08:17:08 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Thomas Theocharis on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
